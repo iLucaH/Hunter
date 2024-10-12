14675b9255fe10d28e769b37f5466b3cdb7c9c5b
Lucas Hautrive <lucahautrive@gmail.com>
2024-10-12 13:11:18 +1100

    Initial Commit

--- src/main/java/me/ilucah/hunter/Main.java
+++ src/main/java/me/ilucah/hunter/Main.java
@@ -2,10 +2,12 @@ package me.ilucah.hunter;
 
 import me.ilucah.hunter.handler.Game;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game("Hunter 2D", 1980, 1080);
+        Game game = new Game("Hunter 2D", Toolkit.getDefaultToolkit().getScreenSize().width, Toolkit.getDefaultToolkit().getScreenSize().height);
         game.start();
     }
 
--- src/main/java/me/ilucah/hunter/scene/implementation/mainmenu/MainMenuScene.java
+++ src/main/java/me/ilucah/hunter/scene/implementation/mainmenu/MainMenuScene.java
@@ -4,17 +4,19 @@ import me.ilucah.hunter.handler.Handler;
 import me.ilucah.hunter.scene.Scene;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class MainMenuScene extends Scene {
 
     public MainMenuScene(Handler handler) {
         super(handler);
-        System.out.println(width + " g" + height);
-        System.out.println(Toolkit.getDefaultToolkit().getScreenSize().width + " " + Toolkit.getDefaultToolkit().getScreenSize().height);
     }
 
     @Override
     public void tick() {
+        if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_P)) {
+            Scene.setScene(handler.getGame().getGameScene());
+        }
 
     }
 
